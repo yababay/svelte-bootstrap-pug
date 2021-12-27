8fc243b07307c2f32c70ec8bba8f8b733e08175a
Михаил Беляков <bigbelk@live.ru>
2021-12-27 14:43:10 +0300

    pug-processing-ok

--- scripts/setupIndex.js
+++ scripts/setupIndex.js
@@ -1,5 +1,6 @@
-const html = require('./html.js')
 const fs = require('fs')
+const html = require('./html.js')
+const settings = require('./settings.js')
 
-fs.writeFileSync('./public/index.html', html())
+fs.writeFileSync('./public/index.html', html(settings))
 
