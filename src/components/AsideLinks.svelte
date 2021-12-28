98fdbefe304ab94fea73472a45c438f6ea33371b
Михаил Беляков <bigbelk@live.ru>
2021-12-28 17:34:05 +0300

    aside-links

---
+++ src/components/AsideLinks.svelte
@@ -0,0 +1,13 @@
+<script>
+    import settings from '../settings.json'
+    import Icon from '@yababay67/svelte-meets-bootstrap-icons/book.svelte'
+</script>
+
+{#each settings.asideLinks as link}
+    <li>
+        <a href={link.link} class="nav-link link-secondary">
+            <Icon size="24" />
+            {link.title}
+        </a>
+    </li>
+{/each}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import useHashRouting from './util/router.js'
 import NavbarIcons from './components/NavbarIcons.svelte'
+import AsideLinks  from './components/AsideLinks.svelte'
 import settings from './settings.json'
 
 if(settings.navbarIcons){
     new NavbarIcons({
         target: document.querySelector('nav ul')
+    })
+}
+
+if(settings.asideLinks){
+    new AsideLinks({
+        target: document.querySelector('aside ul')
     })
 }
 
