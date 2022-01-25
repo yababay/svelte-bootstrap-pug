64fee2deb8d0996f616d9ed943f861e583826195
Михаил Беляков <bigbelk@live.ru>
2022-01-25 14:40:57 +0300

    tiny-errors

--- src/components/AsideItem.svelte
+++ src/components/AsideItem.svelte
@@ -1,12 +1,13 @@
 <script>
     import BootstrapIcon from './BootstrapIcon.svelte'
-    export let link, icon, title, style
+    export let link, icon, title 
+    export let style = 'secondary'
     const size = 24
     const params = {icon, size}
 </script>
 
 <li>
-    <a href={link} class={`nav-link link-${style || 'secondary'}`}>
+    <a href={link} class={`nav-link link-${style}`}>
         <BootstrapIcon {...params}/>
         {title}
     </a>
